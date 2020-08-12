f71b9f20eb7c8eea65371ca41f96c83625a7978b
Olavo <licursiolavo@hotmail.com>
2020-08-12 16:57:38 -0300

    Criação da Entidades

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        //Um usuario pode ter muitos pedidos
+        public ICollection<Pedido> Pedidos { get; set; }
+
+
+
+
+
+    }
+}
